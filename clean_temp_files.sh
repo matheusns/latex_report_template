2e7d8fca350aa9b6f647965db0d0de5925c96250
Matheus Nascimento <contamatheusnascimento@gmail.com>
2018-08-20 11:29:26 -0300

    Update to remove sections directory's temp files

--- clean_temp_files.sh
+++ clean_temp_files.sh
@@ -6,6 +6,7 @@ exts="aux bbl blg brf idx ilg ind lof log lol lot out toc synctex.gz"
 if [ -d $arg ]; then
     for ext in $exts; do
          rm -f $arg/*.$ext
+	 rm -f $arg/sections/*.$ext
     done
 else
     for ext in $exts; do
